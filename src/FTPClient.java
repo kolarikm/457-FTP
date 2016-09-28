96319808740662bca55fa06aa475388c737d2e1c
kolarikm <kolarikm@mail.gvsu.edu>
2016-09-28 14:22:15 -0400

    Client command menu started

--- src/FTPClient.java
+++ src/FTPClient.java
@@ -1,28 +1,61 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+
 /**
  * Created by Michael Kolarik on 9/28/16.
  */
+
 public class FTPClient {
 
-    public Socket clientSocket;
+    private Socket clientSocket;
+    private BufferedReader bufferedReader;
 
     public FTPClient() {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private void connect(String server, int port) {
         try {
-            clientSocket = new Socket("35.40.124.189", 5012);
+            clientSocket = new Socket(server, port);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void menu() {
-        while (true) {
-            System.out.println("Please choose a command:\n\n");
-            System.out.println("CONNECT <server name/IP address> <server port>\n");
-            System.out.println("LIST\n");
-            System.out.println("RETR <filename>\n");
-            System.out.println("STOR <filename>\n");
+    private void menu() {
+        while(true) {
+            System.out.println("Please choose a command:\n");
+            System.out.println("CONNECT <server name/IP address> <server port>");
+            System.out.println("LIST");
+            System.out.println("RETR <filename>");
+            System.out.println("STOR <filename>");
             System.out.println("QUIT\n");
+
+            String[] selection = {"INVALID"};
+
+            try {
+                //Split command into parts based on space in order to parse server name, port, and filenames
+                selection = bufferedReader.readLine().split("\\s");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            switch(selection[0]) {
+                case "CONNECT": //connect()
+                    break;
+                case "LIST": //list()
+                    break;
+                case "RETR": //retr();
+                    break;
+                case "STOR": //stor();
+                    break;
+                case "QUIT": //quit();
+                    break;
+                default: //invalid selection
+                    break;
+            }
         }
     }
 
